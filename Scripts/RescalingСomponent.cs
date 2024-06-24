72585f6f5a0a3c287f443deac55b13fea7941299
Artemii0056 <85370202+Artemii0056@users.noreply.github.com>
2024-06-25 00:03:01 +0500

    init

--- Scripts/RescalingСomponent.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class RescalingСomponent : MonoBehaviour
-{
-    [SerializeField] private float _speed = 1;
-
-    private void Update() =>
-        transform.localScale += Vector3.one * _speed * Time.deltaTime;
-}
