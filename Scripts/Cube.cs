be2d434449f2c63b2281ee5801500c9827145f49
Artemii0056 <85370202+Artemii0056@users.noreply.github.com>
2024-09-02 16:37:26 +0500

    updated

--- Scripts/Cube.cs
+++ Scripts/Cube.cs
@@ -1,19 +1,26 @@
 using System;
 using UnityEngine;
 
-[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(
+    typeof(MeshRenderer), 
+    typeof(Rigidbody))]
 public class Cube : MonoBehaviour
 {
     private Color _color;
     private MeshRenderer _meshRenderer;
+    
+    public Rigidbody Rigidbody { get; private set; }
 
     public int ChanсeOfSeparation { get; private set; } = 100;
+
+    public int MaxChanсeOfSeparation { get; private set; } = 100;
 
     public event Action<Cube> Destroyed;
 
     private void Awake()
     {
         _meshRenderer = GetComponent<MeshRenderer>();
+        Rigidbody = GetComponent<Rigidbody>();
     }
 
     public void OnMouseUpAsButton()
@@ -23,11 +30,11 @@ public class Cube : MonoBehaviour
         gameObject.SetActive(false);
     }
 
-    public void Init(Color color, Transform localScale, int chanceOfSeparation)
+    public void Init(Color color, Vector3 scale, int chanceOfSeparation)
     {
         _meshRenderer.material.color = color;
 
-        transform.localScale = localScale.localScale / 2;
+        transform.localScale = scale;
 
         ChanсeOfSeparation = chanceOfSeparation;
     }
